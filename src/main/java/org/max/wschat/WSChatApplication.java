a63debe818875c940c50b133543b3ee58ae617b4
Manish Reddy <mvadala@mobileiron.com>
2019-02-13 12:27:07 +0530

    first commit

---
+++ src/main/java/org/max/wschat/WSChatApplication.java
@@ -0,0 +1,14 @@
+package org.max.wschat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WSChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WSChatApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/org/max/wschat/config/WSConfig.java
@@ -0,0 +1,23 @@
+package org.max.wschat.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@ComponentScan(basePackages = "org.springframework.web.socket.config")
+public class WSConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
+    }
+}
